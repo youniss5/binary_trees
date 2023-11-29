80df36dae752d768fbe59d098ff4d5f6c7930d78
youniss5 <ahmedhyouniss@gmail.com>
2023-11-29 13:58:57 -0800

    tree depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - function that measures 
+ *                                the depth of a node in a binary tree.
+ * @tree: root node pointer of the tree
+ * Return: depth or NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
+
