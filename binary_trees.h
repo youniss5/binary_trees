8b2629dc57c767501a517be6473082a24225a03e
youniss5 <ahmedhyouniss@gmail.com>
2023-11-29 13:30:02 -0800

    delete tree

--- binary_trees.h
+++ binary_trees.h
@@ -1,11 +1,10 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
