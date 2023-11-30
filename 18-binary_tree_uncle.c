5c465e3c7c769c8d9becee6a405bec97fbbb4990
youniss5 <ahmedhyouniss@gmail.com>
2023-11-30 15:31:09 -0800

    uncle node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_uncle - function that finds the uncle of a node
- * @node: node pointer to find uncle 
+ * @node: node pointer to find uncle
  * Return: pointer to uncle node or NULL
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
